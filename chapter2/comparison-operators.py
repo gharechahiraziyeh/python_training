6870d66ccd85cb007d2cc142a4d26c0721ef3552
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-01-10 09:55:24 +0330

    update comparison-operators file

--- chapter2/comparison-operators.py
+++ chapter2/comparison-operators.py
@@ -3,3 +3,15 @@ x = 4
 y = 3
 z = 25
 print(x == 5)
+print(x == 4)
+print(x != 4)
+print(x != 2)
+print(x > 4)
+print(x > 3)
+print(x < 10)
+print(x < 4)
+print(x >= 4)
+print(x >= 5)
+print(x <= 2)
+print(x <= 5)
+print(x <= 5 == z)
