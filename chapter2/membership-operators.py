de4a8255c031098a3242271755566a9efdf109d8
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-01-10 19:59:03 +0330

    add membership-operators file

---
+++ chapter2/membership-operators.py
@@ -0,0 +1,11 @@
+# -------1--------
+s = "reza dolati 01"
+print("r" in s)
+print("y" in s)
+# ---------2-------
+l = [1, 2, 4, 7, 9, "r"]
+print(2 in l)
+print(5 in l)
+print("r" in l)
+print("r" not in l)
+print(5 not in l)
