b0a6bb8686627ee916d04a5648b7eeed3d4c5309
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-01-13 20:56:26 +0330

    add exercise3

--- chapter2/Exercise.py
+++ chapter2/Exercise.py
@@ -8,3 +8,7 @@ x = int(input("Enter x: "))
 y = int(input("Enter y: "))
 s = x * y
 print("masahat=", s)
+# ---------3--------------
+C = int(input("Enter c: "))
+F = C * 1.8 + 32
+print("Farnhait=", F)
