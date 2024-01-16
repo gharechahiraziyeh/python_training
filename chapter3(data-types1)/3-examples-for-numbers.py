80fc32776dc8d44520547f7e6e0beb1aa10944d0
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-01-16 13:12:13 +0330

    add example2 for numbers

--- chapter3(data-types1)/3-examples-for-numbers.py
+++ chapter3(data-types1)/3-examples-for-numbers.py
@@ -2,3 +2,8 @@
 kg = int(input("Enter kg: "))
 g = kg * 1000
 print("g = ", g, "g", sep="")
+# 2-برنامه ای بنویسید که مساحت مثلث را محاسبه کند
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+s = 0.5 * x * y
+print("s= ", s)
