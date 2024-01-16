53dd04acfacdf6e5053099306c7304e270d1c3dd
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-01-16 23:46:46 +0330

    add exercise1 for numbers

---
+++ chapter3(data-types1)/4-exercises-for-numbers.py
@@ -0,0 +1,6 @@
+# 1-برنامه ای بنویسید که شعاع دایره را از کاربر گرفته و محیط و مساحت دایره را حساب کند
+r = int(input("Enter r: "))
+mohit = 2 * 3.14 * r
+masahat = 3.14 * (r ** 2)
+print("mohit= ", mohit)
+print("masahat= ", masahat)
