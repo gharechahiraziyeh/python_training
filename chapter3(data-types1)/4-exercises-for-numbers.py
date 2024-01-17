e691175eb9d4e82ba8ececab84325e298d6893e2
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-01-17 11:58:12 +0330

    add exercise2 for numbers

--- chapter3(data-types1)/4-exercises-for-numbers.py
+++ chapter3(data-types1)/4-exercises-for-numbers.py
@@ -4,3 +4,9 @@ mohit = 2 * 3.14 * r
 masahat = 3.14 * (r ** 2)
 print("mohit= ", mohit)
 print("masahat= ", masahat)
+# 2-برنامه ای بنویسید که یک عدد را از کاربر گرفته و مربع و مکعب آن را چاپ کند
+x = int(input("Enter x: "))
+x2 = x ** 2
+x3 = x** 3
+print(x, "^", 2, "=", x2)
+print(x, "^", 3, "=", x3)
