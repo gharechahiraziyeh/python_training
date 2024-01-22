a9170ae8603c84b54f8760364692c8e3ec987e4f
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-01-22 20:23:50 +0330

    add string-part1 file

---
+++ chapter3(data-types1)/5-string-part1.py
@@ -0,0 +1,21 @@
+# ------------1-----------------
+x = 'I\'m'
+print(x)
+# -------------2-----------------
+x = 'reza \ndolati'
+print(x)
+# --------------3-----------------------
+x = 'C:\Downloads\\new'
+print(x)
+# --------------4-----------------
+x = r'C:\Downloads\new\telegram'
+print(x)
+# --------------5----------------
+x = """reza dolati
+neda mohammadi
+ali jafari"""
+print(x)
+# -------------6-----------------
+print('reza' + ' ' + 'dolati')
+print('reza' * 3)
+print(4 * 'reza ' + 'dolati')
