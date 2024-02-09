29d21e269669fdcf1afab047fb2dea37ef17750b
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-02-09 14:18:53 +0330

    add exercise1 for string

---
+++ chapter3(data-types1)/13-exercises-for-strings.py
@@ -0,0 +1,10 @@
+# یک رشته را از کاربر بگیرید و تعداد جملات، تعداد کلمات، تعداد کل کارکترها و تعداد حروف انگلیسی را چاپ کند
+s = input("Enter string: ")
+sentences = s.count(".") + s.count("?") + s.count("!")
+words = s.count(" ") + 1
+characters = len(s)
+letters = characters - (s.count(".") + s.count("?") + s.count("!") + s.count(";") + s.count(":") + s.count("-"))
+print("number of sentences: ", sentences)
+print("number of words: ", words)
+print("number of characters: ", characters)
+print("number of letters: ", letters)
