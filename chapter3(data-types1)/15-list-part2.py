e0109fb9043aaf79970c35e91c4792a5023fb815
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-02-17 20:42:26 +0330

    update list-part2 file

--- chapter3(data-types1)/15-list-part2.py
+++ chapter3(data-types1)/15-list-part2.py
@@ -32,3 +32,39 @@ print(x[2])
 print(x[2][0])
 print(x[2][1])
 print(x[2][2][0])
+# ------------5------------------
+x = [1, 2, ['reza', 'dolati'], 3]
+print(x[2][1][0])
+print(x[2][1][-1])
+# -----------6----------------
+l1 = [1, 2, ["a", "b"]]
+l2 = l1.copy()
+print(id(l1))
+print(id(l2))
+l2[2][0] = 0
+print(l1)
+print(l2)
+print(id(l1))
+print(id(l2))
+# --------------7---------
+import copy
+l1 = [1, 2, ["a", "b"]]
+l2 = copy.copy(l1)
+print(id(l1))
+print(id(l2))
+l2[2][0] = 0
+print(l1)
+print(l2)
+print(id(l1))
+print(id(l2))
+# ------------------8---------------
+import copy
+l1 = [1, 2, ["a", "b"]]
+l2 = copy.deepcopy(l1)
+print(id(l1))
+print(id(l2))
+l2[2][0] = 0
+print(l1)
+print(l2)
+print(id(l1))
+print(id(l2))
