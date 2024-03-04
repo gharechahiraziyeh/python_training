ce21607b079a5bd42d0c281119e7848434e0673e
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-03-04 20:25:28 +0330

    add and-or-with-datatypes file

---
+++ chapter3(data-types1)/25-and-or-with-datatypes.py
@@ -0,0 +1,14 @@
+# ------------1----------------
+print(5 and 6)
+print(5 or 6)
+# print(x and y):if x false -> x --- if x true -> y
+# print(x or y): if x false -> y --- if x true -> x
+# -----------2--------------
+x = 5
+y = 10
+print(5 or 10)
+print(5 and x < 10)
+print(0 and x < 10)
+print([] or y == 10)
+print([] or y == 11)
+print(45 or y == 11)
