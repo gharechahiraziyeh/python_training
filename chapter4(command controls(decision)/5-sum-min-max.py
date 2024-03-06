638a76a0405fa6cd59b6fd06d05dc8fc8eeced30
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-03-06 21:03:20 +0330

    add sum-min-max file

---
+++ chapter4(command controls(decision)/5-sum-min-max.py
@@ -0,0 +1,9 @@
+# -----------1-----------
+print(min(1, 5, 6, 7, 8, 9))
+print(min([0, 5, 6, 7, 8, 9], default=567))
+print(min([], default=567))
+print(max("reza", "ali", "neda", "sahel"))
+print(min("reza", "ali", "neda", "sahel"))
+print(sum([1, 2, 3]))
+print(sum((1, 2, 3)))
+print(sum({1, 2, 3}, start=10))
