7f07a856a870d53627ded4a1fa4aef919d77d7c3
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-03-13 14:51:44 +0330

    add range file

---
+++ chapter5(command controls(repitition))/6-range.py
@@ -0,0 +1,16 @@
+# ---------1---------
+for i in range(10):
+    print(i)
+# ------------2----------
+for i in range(2, 10):
+    print(i)
+# -----------3-------------
+for i in range(2, 10, 2):
+    print(i)
+# -----------4-----------
+for i in range(-20, -150, -20):
+    print(i)
+# ----------5------------
+l = input("names: ").split("-")
+for i in range(0, len(l)):
+    print(l[i])
