4b58dc1be0a52d8ebbc7e01f11183cf58c0bbcca
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-03-13 22:44:41 +0330

    add range file

--- chapter5(command controls(repitition))/6-range.py
+++ chapter5(command controls(repitition))/6-range.py
@@ -13,4 +13,28 @@ for i in range(-20, -150, -20):
 # ----------5------------
 l = input("names: ").split("-")
 for i in range(0, len(l)):
-    print(l[i])
+    print(i, l[i])
+# --------6--------------
+n = int(input("n: "))
+m = 1
+for i in range(1, n + 1):
+    m *= i
+print("f: ", m)
+# ------------7-----------
+n = input("n: ")
+s = ""
+for i in range(len(n) - 1, -1, -1):
+    s += n[i]
+x = int(s)
+print(type(x))
+print(s)
+# ------------8---------
+n = int(input("n: "))
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+for i in range(n - 1, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
