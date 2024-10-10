2219954fb63faafaf4232355fc0f3a0f340281d8
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-10-10 20:17:05 +0330

    add argument-syntax file

--- chapter6(function)/4-parameter-syntax.py
+++ chapter6(function)/4-parameter-syntax.py
@@ -33,3 +33,40 @@ def func(a, b, c=10):
     print("c=", c)
 
 func(b=7, a=0)
+#------------------6-*name----------------
+def func(a, b, *c):
+    print("a=", a)
+    print("b=", b)
+    print("c=", c)
+
+func(1, 2, 3, 4, 5, 6)
+#------------------7-*name----------------
+def func(a, b, *c, d):
+    print("a=", a)
+    print("b=", b)
+    print("c=", c)
+    print("d=", d)
+
+func(1, 2, 3, 4, 5, 6, d=0)
+#-----------------------8-**name---------------
+def func(**a):
+    print("a=", a)
+
+
+func(a=1, b=2, c=3)
+
+#------------------9-**name-------------------
+def func(a, **b):
+    print("a=", a)
+    print("b=", b)
+
+
+func(a=1, b=2, c=3)
+#----------------10-**name------------------
+def func(a, b=0, *c, **d):
+    print("a=", a)
+    print("b=", b)
+    print("c=", c)
+    print("d=", d)
+
+func(2, 3, 4, 6, 7, 8, c=0, d=9)
