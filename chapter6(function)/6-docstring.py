d735c0b07a1c951ae7b497d99cb1df6fc9a061a2
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-10-11 11:47:32 +0330

    add docstring file

---
+++ chapter6(function)/6-docstring.py
@@ -0,0 +1,15 @@
+#----------------------1----------------------
+def max3(x, y, z):
+    """Receives three number as input and return the largest of them
+
+    parameters:
+        x (int): A decimal integer
+        y (int): Another decimal integer
+        z (int): Another decimal integer
+
+        Returns:
+            max_int (int): largest of three numbers
+        """
+    return max(x, y, z)
+
+print(max3.__doc__)
